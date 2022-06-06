8bf3a80839e62abefd3f022c6691426f536c4235
Jurema Vidal <juremavidal.jv62@gmail.com>
2022-06-06 12:14:59 +0200

    Edited files

--- 1.00.timing.js
+++ 1.00.timing.js
@@ -1,331 +1,328 @@
 const canvas = document.querySelector(`canvas`);
-const webgl = canvas.getContext(`webgl`); 
-if(!webgl){
-    throw new Error ("WebGL not supported");
-};
+const webgl = canvas.getContext(`webgl`);
+if (!webgl) {
+  throw new Error("WebGL not supported!");
+}
 webgl.clearColor(1.0, 1.0, 1.0, 1.0);
+//webgl.clearColor( Math.random(), Math.random(), Math.random(), 1.0,);
 webgl.clear(webgl.COLOR_BUFFER_BIT);
 webgl.enable(webgl.DEPTH_TEST);
+var r = 0.5;
 
-
-var r = 0.5
+let image = document.getElementById("Me");
 
 var vertices = new Float32Array([
- -r, r, r, 1,0,0, -r,-r, r, 1,0,0,  r,-r,r, 1,0,0,   
- -r, r, r, 1,0,0, -r,-r, r, 1,0,0,  r,-r,r, 1,0,0,  
 
- r, r, r, 1,0,0,  r,-r, r, 1,0,0,  r,r,-r, 1,0,0,   
- r,-r, r, 1,0,0 , r,r, -r, 1,0,0,  r,-r,-r, 1,0,0, 
+  //1.FRONT FACE 
+  r,r,r,   r,-r,r,  -r,r,r,  //1st Triang
+ -r,r,r,   r,-r,r,  -r,-r,r,  //2nd Triang
+  
+  //2.BACK FACE 
+  r,r,-r,  r,-r,-r,  -r,r,-r,   //1st Triang
+ -r,r,-r,  r,-r,-r,   -r,-r,-r, //2nd Triang
 
- -r, r, -r, 1,0,0,  -r,-r,-r, 1,0,0,  r,-r,-r, 1,0,0,   
- r, r,-r, 1,0,0,  -r,-r,-r, 1,0,0,  r,-r,-r, 1,0,0, 
+  //3.TOP FACE
+  r,r,r,   -r,r,r,   -r,r,-r,  //1st Triang
+  r,r,r,  -r,r,-r,   r,r,-r, //2nd Triang
 
- -r, r, r, 1,0,0,  -r,-r, -r, 1,0,0,  -r,r,-r, 1,0,0,   
- -r, -r, r, 1,0,0,  -r,-r, -r, 1,0,0,  -r,-r,-r, 1,0,0, 
+  //4.BOTTON FACE 
+  r,-r,r,  -r,-r,r,   -r,-r,-r,   //1st Triang
+  r,-r,r,  -r,-r,-r,   r,-r,-r,  //2nd Triang
+
+ //5.LEFT FACE 
+ -r,-r,r,  -r,r,r,   -r,-r,-r, //1st Triang
+ -r,-r,-r, -r,r,r,  -r,r,-r,   //2nd Triang
+
+  //6.RIGHT FACE 
+  r,-r,r,   r,r,r,  r,-r,-r,  //1st Triang
+  r,-r,-r,  r,r,r,  r,r,-r,  //2nd Triang
+ ]);
+
  
- -r,-r, r, 1,0,0,  r,-r, r, 1,0,0,  -r,-r,-r, 1,0,0,   
- -r,-r,-r, 1,0,0,  r,-r,-r, 1,0,0,  r,-r,r, 1,0,0, 
+var texCoords = new Float32Array([
 
- -r, r, r, 1,0,0,  r,r, r, 1,0,0,  -r,r,-r, 1,0,0,   
- -r, r,-r, 1,0,0,  r,r,-r, 1,0,0,  r,r,r, 1,0,0, 
+//1.FRONT FACE
+1,1, 1,0, 0,1, 0,1, 1,0, 0,0,
 
+//2.BACK FACE
+1,1, 1,0, 0,1,   0,1, 1,0, 0,0,
 
+//3.TOP FACE
+1,1, 1,0, 0,1,   0,1, 1,0, 0,0,   
 
-// // u,u, u,-u, -u,u, 
-// // u,-u, -u,u, -u,-u,
+//4.BOTTOM FACE
+1,1, 1,0, 0,1,   0,1, 1,0, 0,0,    
 
-// // ]);
+//5.LEFT FACE  
+1,1, 1,0, 0,1,   0,1, 1,0, 0,0,
 
-// // var colours = new Float32Array([
+//6.RIGHT FACE
+1,1, 1,0, 0,1,   0,1, 1,0, 0,0,
+      
+        
+      ]);
+      
 
-//  1.0, 0.0, 0.0, 1.0,
-//  1.0, 0.0, 0.0, 1.0,
-//  1.0, 0.0, 0.0, 1.0,
-
-//  1.0, 0.0, 0.0, 1.0,
-//  1.0, 0.0, 0.0, 1.0,
-//  1.0, 0.0, 0.0, 1.0
- 
-    
-]);
-
-const cube = new Float32Array( vertices);
-
-const positionBuffer= webgl.createBuffer();
-webgl.bindBuffer(webgl.ARRAY_BUFFER, positionBuffer );
+const positionBuffer = webgl.createBuffer();
+webgl.bindBuffer(webgl.ARRAY_BUFFER, positionBuffer);
 webgl.bufferData(webgl.ARRAY_BUFFER, vertices, webgl.STATIC_DRAW);
 
-// const colourBuffer = webgl.createBuffer();
-// webgl.bindBuffer(webgl.ARRAY_BUFFER, colourBuffer );
-// webgl.bufferData(webgl.ARRAY_BUFFER, colours , webgl.STATIC_DRAW);
+const textCoordBuffer = webgl.createBuffer();
+webgl.bindBuffer(webgl.ARRAY_BUFFER, textCoordBuffer);
+webgl.bufferData(webgl.ARRAY_BUFFER, texCoords, webgl.STATIC_DRAW);
+
+const texturebuffer = webgl.createTexture();
+webgl.bindTexture(webgl.TEXTURE_2D, texturebuffer);
+webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_MAG_FILTER, webgl.LINEAR);
+webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_MIN_FILTER, webgl.LINEAR);
+webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_WRAP_S, webgl.CLAMP_TO_EDGE);
+webgl.texParameteri(webgl.TEXTURE_2D, webgl.TEXTURE_WRAP_T, webgl.CLAMP_TO_EDGE);
+webgl.pixelStorei(webgl.UNPACK_FLIP_Y_WEBGL, true);
+
+webgl.texImage2D(webgl.TEXTURE_2D, 0, webgl.RGBA,webgl.RGBA,webgl.UNSIGNED_BYTE, image);
+
 
 const vertexShader = webgl.createShader(webgl.VERTEX_SHADER);
 webgl.shaderSource(
-    vertexShader,
-    ` attribute vec3 pos;
-    attribute vec3 colour;
-    varying vec3 fragcolour;
-    uinform mat4 model
-    uniform float x;
-    uniform float y;
-    voind main(){
-        fragcolour = colour;
+  vertexShader,
+  `attribute vec3 pos;
+attribute vec2 vtexture;
+varying vec2 fragtexture;
+uniform mat4 model1;
+uniform float x;
+uniform float y;
 
-        gl_Position = model*vec4(0.5*pos,1) + vec4(x, y, 0.0, 0.0);
+void main() { 
+    
+    
+    fragtexture = vtexture;
+    
+    gl_Position = model1*vec4(0.5*pos,1) + vec4(x, y, 0.0, 0.0);
 
-}` );
-
+}`
+);
 webgl.compileShader(vertexShader);
-if (!webgl.getShaderParameter(vertexShader, webgl.COMPILE_STATUS)){
-console.error( 
-    "ERROR compiling vertex shader", webgl.getShaderInfoLog(vertexShader)
+if (!webgl.getShaderParameter(vertexShader, webgl.COMPILE_STATUS)) {
+  console.error(
+    "ERROR compiling vertex shader!",
+    webgl.getShaderInfoLog(vertexShader)
+  ); }
 
-);}
-
-const fragmentShader = webgl.createShader(webgl.VERTEX_SHADER);
+const fragmentShader = webgl.createShader(webgl.FRAGMENT_SHADER);
 webgl.shaderSource(
-    fragmentShader,
-    ` precision mediump float;
-    varying vec3 fragcolour;
+  fragmentShader,
+  `precision mediump float; 
+varying vec2 fragtexture;
+uniform sampler2D fragsampler;
 
-    voind main(){
+void main() 
+{
 
-        gl_FragColor = vec4(fragcolour, 1.0);
+    gl_FragColor = texture2D(fragsampler, fragtexture);
 
-}` );
-
+}`);
 webgl.compileShader(fragmentShader);
-if (!webgl.getShaderParameter(fragmentShader, webgl.COMPILE_STATUS)){
-console.error( 
-    "ERROR compiling vertex shader", webgl.getShaderInfoLog(fragmentShader)
-
-);}
+if (!webgl.getShaderParameter(fragmentShader, webgl.COMPILE_STATUS)) {
+  console.error(
+    "ERROR compiling fragment shader!",
+    webgl.getShaderInfoLog(fragmentShader)
+  ); }
 
 const program = webgl.createProgram();
 webgl.attachShader(program, vertexShader);
 webgl.attachShader(program, fragmentShader);
 webgl.linkProgram(program);
 webgl.useProgram(program);
+webgl.enable(webgl.DEPTH_TEST);
 
-const  positionLocation =  webgl.getAttribLocation(program, `pos`);
+const positionLocation = webgl.getAttribLocation(program, `pos`);
 webgl.enableVertexAttribArray(positionLocation);
-webgl.vertexAttribPointer(positionLocation, 3, webgl.FLOAT, false, 6*4, 0);
-
-const  coloursLocation =  webgl.getAttribLocation(program, `colour`);
-webgl.enableVertexAttribArray(coloursLocation);
-webgl.vertexAttribPointer(coloursLocation, 3, webgl.FLOAT, false, 6*4, 3*4);
+webgl.bindBuffer(webgl.ARRAY_BUFFER, positionBuffer);
+webgl.vertexAttribPointer(positionLocation, 3, webgl.FLOAT,false, 0, 0);
 
 
-let model = creatmat4();
-let model2 = creatmat4();
-let b = 0; //Math.PI/8;
+const textureLocation = webgl.getAttribLocation(program, `vtexture`);
+webgl.enableVertexAttribArray(textureLocation );
+webgl.bindBuffer(webgl.ARRAY_BUFFER, textCoordBuffer);
+webgl.vertexAttribPointer(textureLocation, 2, webgl.FLOAT, false, 0, 0);
+
+
+let model1 = createmat4();
+let model2 = createmat4();
+let b = 0; //Math.PI;
 let xt = -0.8;
-let yt =  0.0;
-let zt = 0.0;
-let xtt = 0.5;
-let ytt = 0.0;
-let ztt = 0.0;
+let yt = 0;
+let zt = 0;
+let out42 = 0.5;
+let out52 = 0.0;
+let out62 = 0.0;
+stopstart = 1;
 let isSpinning = false;
-//let interval = null;
-let spinningAngle = Math.PI/8;
-//let interval = null;
+let interval = null;
+let spinningAngle = Math.PI /8;
 
-document.querySelectorAll("button").forEach((element) =>{
-    element.onclick = function () {
-        switch (element.id) {
-            case "rtx":
-                model2 = matmult(model2, rotx(Math.PI/8));
-                break;
-
-            case "rty":
-                model2 = matmult(model2, roty(Math.PI/8));
-                break;
-
-            case "rtz":
-                model2 = matmult(model2, rotz(Math.PI/8));
-                break;
-
-            case "spx":
-                    model2 = matmult(model2, rotx(Math.PI/8));
-                    break;
-          
-            case "spy":
-                model2 = matmult(model2, roty(Math.PI/8));
-                break;
-          
-            case "spz":
-                model2 = matmult(model2, rotz(Math.PI/8));
-                break;
-
-            case "spup":
-                    spinningAngle += Math.PI; //Math.PI / 8;
-                    break;
-
-            case "spdw":
-                    spinningAngle -= Math.PI;
-                    break;
-                
-            // case "spup":
-            //     model2 = matmult(model2, roty(Math.PI/8));
-            //     break;
-          
-            // case "spdw":
-            //     model2 = matmult(model2, roty(Math.PI/8));
-            //     break;
-          
-        } 
-    };
+document.querySelectorAll("button").forEach((element) => {
+  element.onclick = function () {
+    switch (element.id) {
+      case "rotX":
+        model2 = matmult(model2, rotx(Math.PI / 8));
+        break;
+      case "rotY":
+        model2 = matmult(model2, roty(Math.PI / 8));
+        break;
+      case "rotZ":
+        model2 = matmult(model2, rotz(Math.PI / 8));
+        break;
+      case "spinX":
+        spin("x");
+        break;
+      case "spinY":
+        spin("y");
+        break;
+      case "spinZ":
+        spin("z");
+        break;
+      case "spinUp":
+        spinningAngle += Math.PI; //Math.PI / 8;
+        break;
+      case "spinDown":
+        spinningAngle -= Math.PI;
+        break;
+    }
+  };
 });
 
 function spin(axis) {
-    isSpinning = !isSpinning;
-    if(isSpinning) {
-        interval = setInterval(() => {
-            switch (axis) {
-                case "x":
-                    model2 = matmult(model2, rotx(spinningAngle));
-                    break;
-                case "y":
-                        model2 = matmult(model2, roty(spinningAngle));
-                    break;
-
-                 case "z":
-                    model2 = matmult(model2, rotz(spinningAngle));
-                    break;
-            }
-
-        }, );
-
-    } else clearInterval(interval);
-    
+  isSpinning = !isSpinning;
+  if (isSpinning) {
+    interval = setInterval(() => {
+      switch (axis) {
+        case "x":
+          model2 = matmult(model2, rotx(spinningAngle));
+          break;
+        case "y":
+          model2 = matmult(model2, roty(spinningAngle));
+          break;
+        case "z":
+          model2 = matmult(model2, rotz(spinningAngle));
+      }
+    }, 150);
+  } else clearInterval(interval);
 }
 
+spinX.addEventListener("click", function () {
+  matmult(createmat4(), rotx(a));
+});
+
 document.onkeydown = function (event) {
-   switch (event.key) {
-       case `ArrowRight`:
-           xt += 0.01;
-           break;
-
-        case `ArrowLeft`:
-            xt -= 0.01;
-            break;  
-        
-        case `ArrowUp`:
-            yt += 0.01;
-            break; 
-
-        case `ArrowDown`:
-            yt -= 0.01;
-            break;
-   
-        }
-   
+  switch (event.key) {
+    //For model1
+    case `ArrowRight`: //translate right along the x-axis
+      xt += 0.01;
+      break;
+    case `ArrowLeft`: //translate left along the x-axis
+      xt -= 0.01;
+      break;
+    case `ArrowUp`: // translate up along the y-axis
+     yt += 0.01;
+      break;
+    case `ArrowDown`: // translate down along the y-axis
+      yt -= 0.01;
+      break;
+  }
 };
 
 draw();
 
 function draw() {
+  
+  webgl.clear(webgl.COLOR_BUFFER_BIT);
+  model1 = matmult(createmat4(), translate(xt, yt, zt));
+  webgl.uniformMatrix4fv(webgl.getUniformLocation(program, `model1`),false,model1);
+  webgl.drawArrays(webgl.TRIANGLES, 0, 36);
+  webgl.uniformMatrix4fv(webgl.getUniformLocation(program, `model1`),false, model2);
+  webgl.uniform1f(webgl.getUniformLocation(program, "y"), 0.0);
+  webgl.uniform1f(webgl.getUniformLocation(program, "x"), 0.5);
+  webgl.activeTexture(webgl.TEXTURE0);
+  webgl.drawArrays(webgl.TRIANGLES, 0, 36);
+  window.requestAnimationFrame(draw);
+}
+ 
+//vertices.length/6
 
-    webgl.clear(webgl.COLOR_BUFFER_BIT);
-    model= matmult(createmat4(), translate(xt, yt, zt));
-    webgl.uniformMatrix4fv(webgl.getUniformLocation(program, `model`), false, model);
-    webgl.drawArrays(webgl.TRIANGLES, 0, vertices.length/6);
-    webgl.uniformMatrix4fv(webgl.getUniformLocation(program, `model`), false, model2);
-    webgl.uniform1f.getUniformLocation((program, "x"), 0.5);
-    webgl.uniform1f.getUniformLocation((program, "y"), 0.0);
-    webgl.drawArrays(webgl.TRIANGLES, 0, vertices.length/6);
-    window.requestAnimationFrame(draw);
-
-};
-
-// function rotx(angle) {
-//     var c = Math.cos(angle);
-//     var s = Math.sin(angle);
-//     return new Float32Array([
-//         c,-s, 0, 0,
-//         c,-s, 0, 0,
-//         0, 0, 1, 0,
-//         0, 0, 0, 1,
-//     ]);
-// }
-
-function rotx(angle) {
-    var c = Math.cos(angle);
-    var s = Math.sin(angle);
-    return new Float32Array([
-        1, 0, 0, 0,
-        0, c, s, 0,
-        0,-s, c, 0,
-        0, 0, 0, 1,
-    ]);
+// Rotation Matrix along X-Axis
+function rotx(angleInRadians) {
+  var c = Math.cos(angleInRadians);
+  var s = Math.sin(angleInRadians);
+  return new Float32Array([
+    1, 0, 0, 0, 
+    0, c, s, 0,
+    0,-s, c, 0, 
+    0, 0, 0, 1]);
 }
 
-function roty(angle) {
-    var c = Math.cos(angle);
-    var s = Math.sin(angle);
-    return new Float32Array([
-        c, 0,-s, 0,
-        0, 1, 0, 0,
-        s, 0, c, 0,
-        0, 0, 0, 1,
-    ]);
+// Rotation Matrix along Y-Axis
+function roty(angleInRadians) {
+  var c = Math.cos(angleInRadians);
+  var s = Math.sin(angleInRadians);
+  return new Float32Array([
+    c, 0,-s, 0, 
+    0, 1, 0, 0,
+    s, 0, c, 0,
+    0, 0, 0, 1]);
 }
 
-function rotz(angle) {
-    var c = Math.cos(angle);
-    var s = Math.sin(angle);
-    return new Float32Array([
-        c,-s, 0, 0,
-        s, c, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1,
-    ]);
+// Rotation Matrix along Z-Axis
+function rotz(angleInRadians) {
+  var c = Math.cos(angleInRadians);
+  var s = Math.sin(angleInRadians);
+  return new Float32Array([
+    c,-s, 0, 0, 
+    s, c, 0, 0, 
+    0, 0, 1, 0, 
+    0, 0, 0, 1]);
 }
 
+// Translation Matrice along X-Y Axis
 function translate(tx, ty, tz) {
-    return new Float32Array([
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        tx,ty,tz,1,
-    ]);
+  return new Float32Array([
+    1, 0, 0, 0, 
+    0, 1, 0, 0,
+    0, 0, 1, 0, 
+    tx,ty,tz,1]);
 }
+// Identity Matrix
+function createmat4() {
+  return new Float32Array([
+    1, 0, 0, 0, 
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1,
 
-function creatmat4() {
-    return new Float32Array([
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1,
-    ]);
+    //m[1,0,0,0,  0,1,0,0,0, 0,1,0 0,0,0,1]
+  ]);
 }
-
-    
 
 function matmult(model, b) {
-    return[
-        model[0] * b[0] + model[1] * b[4] + model[2]* b[8] + model[3]* b[12],
-        model[0] * b[1] + model[1] * b[5] + model[2]* b[9] + model[3]* b[13],
-        model[0] * b[2] + model[1] * b[6] + model[2]* b[10] + model[3]* b[14],
-        model[0] * b[3] + model[1] * b[7] + model[2]* b[11] + model[3]* b[15],
+  return [
+    model[0] * b[0] + model[1] * b[4] + model[2] * b[8] + model[3] * b[12],
+    model[0] * b[1] + model[1] * b[5] + model[2] * b[9] + model[3] * b[13],
+    model[0] * b[2] + model[1] * b[6] + model[2] * b[10] + model[3] * b[14],
+    model[0] * b[3] + model[1] * b[7] + model[2] * b[11] + model[3] * b[15],
 
-        model[4] * b[0] + model[5] * b[4] + model[6]* b[8] + model[7]* b[12],
-        model[4] * b[1] + model[5] * b[5] + model[6]* b[9] + model[7]* b[13],
-        model[4] * b[2] + model[5] * b[6] + model[6]* b[10] + model[7]* b[14],
-        model[4] * b[3] + model[5] * b[7] + model[6]* b[11] + model[7]* b[15],
+    model[4] * b[0] + model[5] * b[4] + model[6] * b[8] + model[7] * b[12],
+    model[4] * b[1] + model[5] * b[5] + model[6] * b[9] + model[7] * b[13],
+    model[4] * b[2] + model[5] * b[6] + model[6] * b[10] + model[7] * b[14],
+    model[4] * b[3] + model[5] * b[7] + model[6] * b[11] + model[7] * b[15],
 
-        model[8]* b[0] + model[9] * b[4] + model[10]* b[8] + model[11]* b[12],
-        model[8] * b[1] + model[9] * b[5] + model[10]* b[9] + model[11]* b[13],
-        model[8] * b[2] + model[9] * b[6] + model[10]* b[10] + model[11]* b[14],
-        model[8] * b[3] + model[9] * b[7] + model[10]* b[11] + model[11]* b[15],
+    model[8] * b[0] + model[9] * b[4] + model[10] * b[8] + model[11] * b[12],
+    model[8] * b[1] + model[9] * b[5] + model[10] * b[9] + model[11] * b[13],
+    model[8] * b[2] + model[9] * b[6] + model[10] * b[10] + model[11] * b[14],
+    model[8] * b[3] + model[9] * b[7] + model[10] * b[11] + model[11] * b[15],
 
-        model[12] * b[0] + model[13] * b[4] + model[14]* b[8] + model[15]* b[12],
-        model[12] * b[1] + model[13] * b[5] + model[14]* b[9] + model[15]* b[13],
-        model[12] * b[2] + model[13]* b[6] + model[14]* b[10] + model[15]* b[14],
-        model[12] * b[3] + model[13]* b[7] + model[14]* b[11] + model[15]* b[15],
-        
-        
-
-    ];
-    
+    model[12] * b[0] + model[13] * b[4] + model[14] * b[8] + model[15] * b[12],
+    model[12] * b[1] + model[13] * b[5] + model[14] * b[9] + model[15] * b[13],
+    model[12] * b[2] + model[13] * b[6] + model[14] * b[10] + model[15] * b[14],
+    model[12] * b[3] + model[13] * b[7] + model[14] * b[11] + model[15] * b[15],
+  ];
 }
